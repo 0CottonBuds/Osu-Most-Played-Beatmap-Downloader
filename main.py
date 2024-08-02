25895ac4eec6e8ba339477c33c3a22f628ed24c5
unknown <emiljohnllanes772@gmail.com>
2024-08-02 13:05:39 +0800

    refactored code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
+import time
 
 def download_single_beatmap(beatmap_id, song_title):
+    '''downloads single beatmap by beatmap id and song title'''
+    
     headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", 
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 OPR/108.0.0.0",
@@ -11,57 +14,117 @@ def download_single_beatmap(beatmap_id, song_title):
     "Upgrade-Insecure-Requests": "1"}
     response: requests.Response = requests.get(f"https://api.chimu.moe/v1/download/{beatmap_id}?n=1", allow_redirects=True)
 
-    if response.status_code == 200:
-        with open(f"{beatmap_id} - {song_title}.osz", "wb") as osx:
-            osx.write(response.content)
-        return response.status_code
-    else:
-        print(f"Failed to download {beatmap_id}, {song_title}" )
-        return beatmap_id 
+    if not response.status_code == 200:
+        print(f"API ERROR:{response.status_code} \nFailed to download {beatmap_id}, {song_title}." )
+        return False 
+    
+    try:
+        with open(f"./Downloads/{beatmap_id} - {song_title}.osz", "wb") as file:
+                file.write(response.content)
+        return True
+    except:
+        print(f"Error while downloading {beatmap_id}, {song_title}!" )
+        return False
 
 def download_beatmaps(beatmaps: list[dict]):
+    '''
+    Tries to download list of beatmaps(JSON from osu! API call)\n
+    Creates a txt file containing all failed downloads
+    '''
+    
     progress = 0 
     failed_downloads = []
+
     try:
         for beatmap in beatmaps:
             beatmapset_id = beatmap["beatmap"]["beatmapset_id"] 
             song_title = beatmap["beatmapset"]["title"]
             print(f"Downloading {beatmapset_id} - {song_title}.") 
-            is_success = download_single_beatmap(beatmapset_id, song_title)
-            if is_success == 0:
+
+            isSuccess: bool = download_single_beatmap(beatmapset_id, song_title)
+            if isSuccess: # success
                 progress += 100/len(beatmaps)
                 print(f"Progress: {progress}%")
             else:
-                failed_downloads.append(is_success)
+                failed_downloads.append(beatmapset_id)
     except:
-        print(f"Error downloading beatmaps")
-    finally:
-        with open("failed_downloads1.txt", "wb") as failed_downloads_txt:
+        print(f"Unexpected error occurred! \nSome beatmaps may not be downloaded. see failed_downloads.txt for list of beatmap id of failed downloads.")
+        with open("failed_downloads.txt", "wb") as failed_downloads_txt:
             for failed_download in failed_downloads:
                 failed_downloads_txt.write(failed_download)
             failed_downloads_txt.flush()
+        return
 
-def retrieve_most_played_beatmaps(user_id:str, limit:int, offs:int = 0) -> list[dict]:
+    print("Finished downloading all beatmaps. Some beatmaps may not be downloaded. see failed_downloads.txt for list of beatmap id of failed downloads")
+    with open("failed_downloads.txt", "wb") as failed_downloads_txt:
+        for failed_download in failed_downloads:
+            failed_downloads_txt.write(failed_download)
+        failed_downloads_txt.flush()
+
+def retrieve_most_played_beatmaps(user_id:str, limit:int, offs:int = 0, step:int = 10, sleep:float = 2.0) -> list[dict]:
     '''
+    Retrieves most played beatmaps with following parameters\n
+    \n
     user_id = Osu! User ID \n
-    limit = number of entries 
+    limit = number of most played beatmaps to be downloaded \n
+    offs (default 0) = offset from the start of list \n
+    step (default 10, max 40) = number of beatmaps per API call\n
+    sleep (default 2.0) = sleep time per API call. NOTE: you might be ip blocked by osu\n 
+    if you send too much requests at a single time so set this value too low.\n
     '''
-    beatmaps: list[dict] = [] 
-    offset = offs
 
-    for offset in range(offs , limit, 10):
+    beatmaps: list[dict] = [] 
+
+    for offset in range(offs , limit, step):
         try:
-            response: requests.Response = requests.get(url=f"https://osu.ppy.sh/users/{user_id}/beatmapsets/most_played?limit=10&offset={offset}")
+            response: requests.Response = requests.get(url=f"https://osu.ppy.sh/users/{user_id}/beatmapsets/most_played?limit={step}&offset={offset}")
+
             if response.status_code != 200:
                 print(f"encountered status code: {response.status_code}")
                 continue
+
+            print(response.json())
             beatmaps.extend(response.json())
-            download_beatmaps(response.json())
+
+            time.sleep(sleep)
+            
         except:
             print(f"Error encountered at beatmaps {offset} - {offset + 10}")
+            print(f"Returning {len(beatmaps)}")
+            return beatmaps
 
-def main():
-     retrieve_most_played_beatmaps("18453878", 1037, 12)
+    return beatmaps
 
+def check_if_user_exists(userID):
+    print("Checking if user exists")
+    response = requests.get(url=f"https://osu.ppy.sh/users/{userID}")
+
+    if response.status_code != 200:
+        print(f"There is no user with id of: {userID}")
+        print("Exiting...")
+        exit()
+
+
+# main 
 if __name__ == "__main__":
-    main()
+    print(
+"""
+Welcome to osu! most played beatmap downloader. 
+As the name suggest this downloads your most played beatmaps using your user id.
+To start please answer the prompts 
+"""
+        )
+
+    print("User ID can be obtained trough osu! website")
+    user_id = int(input("Enter your user ID: "))
+    check_if_user_exists(user_id)
+
+    print("How many beatmaps would you like to download (number)")
+    limit = int(input("qty: "))
+    print("Where would you want to start downloading (1 if you want to skip the first beatmap)")
+    offset = int(input("Offset: "))
+
+
+    beatmaps = retrieve_most_played_beatmaps(user_id, limit, offset)
+    download_beatmaps(beatmaps)
+
